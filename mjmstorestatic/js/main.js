627e870c7a80fb46af5b9efee561b044914782c8
habsiger <sebastien.habsiger@laposte.net>
2014-02-04 11:54:08 +0100

    4 Février 2014

--- mjmstorestatic/js/main.js
+++ mjmstorestatic/js/main.js
@@ -6,19 +6,28 @@ $('.homeslider').cycle({
 	prev : "#prev"
 });
 
-var windowHeight, windowscrollTop, footerOffset;
+var windowHeight, windowscrollTop, footerOffset, i, j, step;
 
-
+i = 6;
+j = 6;
+step = 1;
 
 $(window).bind('scroll',function(f){
 	windowHeight = $(window).height();
 	windowscrollTop = $(window).scrollTop() + windowHeight;
 	footerOffset = $('.footer_main').offset().top;
+	console.log('footerOffset '+footerOffset);
+	console.log('i '+i);
 
-	if (windowscrollTop > footerOffset) {
-		$('#loading').delay(600).fadeOut(300);
-		$('#wall').delay(900).fadeIn(600);
-	};
+	if (windowscrollTop > footerOffset && step == 1) {
+		step = 0;
+		$('#loading').fadeIn(0);
+		$('#loading').delay(1600).fadeOut(300);
+		$('#wall article:lt('+i+')').delay(1900).fadeIn(600);
+		step = 1;
+		i = i+j;
+	}
+
 });
 
 
